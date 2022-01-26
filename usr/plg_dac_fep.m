0845dccecd084abf191769af27d63a6c05479dac
yingqiwong <52212328+yingqiwong@users.noreply.github.com>
2022-01-26 11:53:25 +0100

    updating plg-dac-mvp permissions and pure-phase properties

--- usr/plg_dac_fep.m
+++ usr/plg_dac_fep.m
@@ -7,14 +7,14 @@ clear; close all; clc;
 % set model parameters
 RunID  = 'plgdacfep';
 outdir = '../out/';         % directory to save output files
-nop    = 1;                 % plot and store output every [nop] time step
-svop   = 0;                 % save output and print figures
+nop    = 10;                % plot and store output every [nop] time step
+svop   = 1;                 % save output and print figures
 restart= 0;
 
 NPHS   = 3;                 % number of phases
 N      = 200;               % number of grid points in each direction
 D      = 100;               % domain dimension in each direction [delta0]
-BC     = 'periodic';          % boundary conditions: 'open', 'closed', 'periodic'
+BC     = 'periodic';        % boundary conditions: 'open', 'closed', 'periodic'
 NtMax  = 1e3;               % maximum number of time steps
 tend   = 1e16;              % model run time [s]
 
@@ -31,20 +31,26 @@ thtlim = 1e+6;              % limit phase-internal permission contrasts
 cfflim = 1e+6;              % limit inter-phase coefficient contrasts
 
 grav = [-9.81,0];           % gravity in vertical and horizontal direction
-f0   = [ 0.10; 0.50; 0.10];  % initial background phase fractions (unity sum!)
+f0   = [ 0.20; 0.70; 0.10]; % initial background phase fractions (unity sum!)
 dfg  = [ 0.00; 0.00; 0.00]; % initial guassian peak amplitude (unity sum!)
 dfr  = [-0.01;-0.01; 0.02]; % initial random perturbation amplitude (unity sum!)
 smth = (N/40)^2;            % smoothing parameter for random perturbation field
 Gmg  = [1;-1;0].*0e-4;      % impose gaussian-shaped mass transfer rate (unity sum!)
 
-rho0 = [3000 ;2500;4000];   % pure-phase densities
-eta0 = [1e+16;1e+3;1e+0];   % pure-phase viscosities
-d0   = [1e-2 ;1e-2;1e-2];   % characteristic size of local-scale phase constituents
+rho0 = [2700 ; 2400; 4000]; % pure-phase densities
+eta0 = [1e+16; 1e+5;    1]; % pure-phase viscosities
+d0   = [1e-3 ; 1e-3; 1e-3]; % characteristic size of local-scale phase constituents
 
 % set permission weight parameters for coefficient closure model
-A = [ 0.60, 0.25, 0.30; 0.20, 0.20, 0.20; 0.20, 0.20, 0.20; ];  % permission slopes
-B = [ 0.30, 0.15, 0.55; 0.48, 0.02, 0.50; 0.80, 0.08, 0.12; ];  % permission step locations
-C = [ 0.20, 0.20, 0.20; 0.60, 0.60, 0.12; 0.20, 0.25, 0.50; ];  % permission step widths
+A  =  [ 0.25, 0.25, 0.25; ...
+        0.25, 0.25, 0.25; ...
+        0.25, 0.25, 0.25; ];  % permission slopes
+B  =  [ 0.44, 0.18, 0.38; ...
+        0.60, 0.03, 0.37; ...
+        0.70, 0.24, 0.06; ];  % permission step locations
+C  =  [ 0.30, 0.30, 0.30; ...
+        0.60, 0.60, 0.12; ...
+        0.60, 0.12, 0.60; ];  % permission step widths
 
 % check problem scales (returns delta0, w0)
 scales;
