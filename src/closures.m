f1803e7328fd00ca447d9ba859f64695aede55e5
Tobias Keller <kellertobs@gmail.com>
2023-06-09 09:34:18 +0200

    move some more expensive calculations from constitutive to closures, adapt pseudo-time step size

--- src/closures.m
+++ src/closures.m
@@ -41,15 +41,20 @@ omfc = Cf./sum(Cf,1);  omfx = Cfx./sum(Cfx,1);  omfz = Cfz./sum(Cfz,1);
 omdp = Kf./sum(Kf,1);  ompx = Kfx./sum(Kfx,1);  ompz = Kfz./sum(Kfz,1);
 
 % update reference and auxiliary fields
-ustar = sum(omvx.*u  ,1);  Du = u-ustar;
-wstar = sum(omvz.*w  ,1);  Dw = w-wstar;
-pstar = sum(omfc.*p  ,1);  Dp = p-pstar;
+ustar = sum(omvx.*u  ,1);  Du = u-ustar;  usegr = fx.*Du;
+wstar = sum(omvz.*w  ,1);  Dw = w-wstar;  wsegr = fz.*Dw;
+pstar = sum(omfc.*p  ,1);  Dp = p-pstar;  pcmpt = f .*Dp;
 
-% advection on reference velocity
+% update advection on reference velocity
 vstar_Gf  = advect(f, ustar, wstar, NUM.h, {NUM.advn, 'vdf'}, [2,3], NUM.BC);
 
-% get iterative pseudo-time steps
-dtau_u = 3./4./( (1-fx+NUM.dmp).*Kvx./(NUM.h/NUM.ndim)^2 + (1-omvx).*Cvx + (1-fx).*(fx.^2./Cfx)./(NUM.h/NUM.ndim)^2 ) ;
-dtau_w = 3./4./( (1-fz+NUM.dmp).*Kvz./(NUM.h/NUM.ndim)^2 + (1-omvz).*Cvz + (1-fz).*(fz.^2./Cfz)./(NUM.h/NUM.ndim)^2 ) ;
-dtau_p = 1./4./( (1-f         ).*Kf ./(NUM.h/NUM.ndim)^2 + (1-omfc).*Cf  + (1-f ).*(f .^2./Cv )./(NUM.h/NUM.ndim)^2 ) ;
+% update auxiliary fields
+Gx_pstar  = sum(ompx.*diff(p(:,:,NUM.icx),1,3)./NUM.h,1);
+Gz_pstar  = sum(ompz.*diff(p(:,NUM.icz,:),1,2)./NUM.h,1);
+pstar_Gfx = (pstar(:,:,NUM.imx)+pstar(:,:,NUM.ipx))./2.*diff(f(:,:,NUM.icx),1,3)./NUM.h;
+pstar_Gfz = (pstar(:,NUM.imz,:)+pstar(:,NUM.ipz,:))./2.*diff(f(:,NUM.icz,:),1,2)./NUM.h;
 
+% get iterative pseudo-time steps
+dtau_u = 0.75./( (1-fx+NUM.dmp).*Kvx./(NUM.h/NUM.ndim)^2 + (1-omvx).*Cvx + (1-fx).*(fx.^2./Cfx)./(NUM.h/NUM.ndim)^2 ) ;
+dtau_w = 0.75./( (1-fz+NUM.dmp).*Kvz./(NUM.h/NUM.ndim)^2 + (1-omvz).*Cvz + (1-fz).*(fz.^2./Cfz)./(NUM.h/NUM.ndim)^2 ) ;
+dtau_p = 0.25./( (1-f         ).*Kf ./(NUM.h/NUM.ndim)^2 + (1-omfc).*Cf  + (1-f ).*(f .^2./Cv )./(NUM.h/NUM.ndim)^2 ) ;
--- src/constitutive.m
+++ src/constitutive.m
@@ -1,13 +1,8 @@
 
-% update reference and auxiliary fields
-ustar     = sum(omvx.*u  ,1); Du = u-ustar;
-wstar     = sum(omvz.*w  ,1); Dw = w-wstar;
-pstar     = sum(omfc.*p  ,1); Dp = p-pstar;
-Gx_pstar  = sum(ompx.*diff(p(:,:,NUM.icx),1,3)./NUM.h,1);
-Gz_pstar  = sum(ompz.*diff(p(:,NUM.icz,:),1,2)./NUM.h,1);
-pstar_Gfx = (pstar(:,:,NUM.imx)+pstar(:,:,NUM.ipx))./2.*diff(f(:,:,NUM.icx),1,3)./NUM.h;
-pstar_Gfz = (pstar(:,NUM.imz,:)+pstar(:,NUM.ipz,:))./2.*diff(f(:,NUM.icz,:),1,2)./NUM.h;
-Div_v     = diff(u,1,3)./NUM.h + diff(w,1,2)./NUM.h;
+% update reference fields
+ustar     = sum(omvx.*u  ,1);
+wstar     = sum(omvz.*w  ,1);
+pstar     = sum(omfc.*p  ,1);
 
 
 % volume flux fields
@@ -16,14 +11,16 @@ qfz = - Kfz.*(diff(p(:,NUM.icz,:),1,2)./NUM.h - Gz_pstar) + fz.*w;
 if strcmp(NUM.BC{1},'closed'); qfz(:,[1,end],:) = 0; end
 if strcmp(NUM.BC{2},'closed'); qfx(:,:,[1,end]) = 0; end
 
-% divergence of volume fluxes (combine diffusive & advective parts)
+% divergence of volume fluxes
 Div_qf = diff(qfx,1,3)./NUM.h + diff(qfz,1,2)./NUM.h;
 
-%get volume transfer fields
+% get volume transfer fields
 Gf  = Cf.*(p-pstar) - vstar_Gf;
 
 
 % momentum flux fields
+Div_v     = diff(u,1,3)./NUM.h + diff(w,1,2)./NUM.h;
+
 qvxx  = - Kv .* (diff(u,1,3)./NUM.h - Div_v./3 + NUM.dmp*sum(Div_qf,1) - PHS.Pu) + f.*p;
 qvzz  = - Kv .* (diff(w,1,2)./NUM.h - Div_v./3 + NUM.dmp*sum(Div_qf,1) + PHS.Pu) + f.*p;
 qvxz  = - Kvn.* (diff(u(:,NUM.icz,:),1,2)./NUM.h + diff(w(:,:,NUM.icx),1,3)./NUM.h + 2*PHS.Si)./2;
